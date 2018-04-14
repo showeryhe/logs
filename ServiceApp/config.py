09be7e31980bbd0d11e383c0443102dd94da6776
showeryhe <showeryhe@hotmail.com>
2018-04-14 09:55:53 +0800

    Update config.py

--- ServiceApp/config.py
+++ ServiceApp/config.py
@@ -2,11 +2,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+	SECRET_KEY = os.environ.get('SECRET_KEY') or '<secret>'
 	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
-	FLASKY_MAIL_SUBJECT_PREFIX = '[Rainbow]'
-	FLASKY_MAIL_SENDER = 'Rainbow Admin <showeryhe@hotmail.com>'
+	FLASKY_MAIL_SUBJECT_PREFIX = '<secret>'
+	FLASKY_MAIL_SENDER = '<secret> <<secret>@hotmail.com>'
 	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
 	@staticmethod
@@ -18,18 +18,18 @@ class DevelopmentConfig(Config):
 	MAIL_SERVER = 'smtp-mail.outlook.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('showeryhe')
-	MAIL_PASSWORD = os.environ.get('hxo@1009')
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://CommonLanguage:19870408@localhost/service'
+	MAIL_USERNAME = os.environ.get('<secret>')
+	MAIL_PASSWORD = os.environ.get('<secret>')
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<secret>:<secret>@localhost/<secret>'
 	# os.environ.get('DEV_DATABASE_URL') or
 	#	'sqlite:////' + os.path.join(basedir, 'data-dev.sqlite')
 
 class TestingConfig(Config):
 	TESTING = True
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://test:test@localhost/test'
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<secret>:<secret>@localhost/<secret>'
 
 class ProductionConfig(Config):
-	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://CommonLanguage:19870408@localhost/service'
+	SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://<secret>:<secret>@localhost/<secret>'
 
 config = {
 	'development': DevelopmentConfig,
